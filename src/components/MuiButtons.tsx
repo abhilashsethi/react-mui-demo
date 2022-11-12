b8b396a1c5653ad81301093d875331c1f25bb656
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2022-11-12 09:45:41 +0530

    muiButtonGroup

--- src/components/MuiButtons.tsx
+++ src/components/MuiButtons.tsx
@@ -1,30 +1,52 @@
 import React from "react";
-import { Button, IconButton, Stack } from "@mui/material";
+import { Button, ButtonGroup, IconButton, Stack } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
 const MuiButtons = () => {
 	return (
-		<Stack spacing={2} direction="row">
-			<Button
-				startIcon={<SendIcon />}
-				variant="text"
-				href="https://www.google.com"
-			>
-				Text
-			</Button>
+		<Stack
+			spacing={4}
+			direction="column"
+			justifyContent={"center"}
+			alignItems={"center"}
+			className="mt-5"
+		>
+			<Stack spacing={2} direction="row">
+				<Button
+					startIcon={<SendIcon />}
+					variant="text"
+					href="https://www.google.com"
+				>
+					Text
+				</Button>
 
-			<Button
-				endIcon={<SendIcon />}
-				variant="contained"
-				onClick={() => alert("clicked")}
-			>
-				contained
-			</Button>
+				<Button
+					variant="contained"
+					endIcon={<SendIcon />}
+					onClick={() => alert("clicked")}
+				>
+					contained
+				</Button>
 
-			<Button variant="outlined">outlined</Button>
-			<IconButton aria-aria-label="send" color="success" size="small">
-				<SendIcon />
-			</IconButton>
+				<Button variant="outlined">outlined</Button>
+				<IconButton aria-aria-label="send" color="success" size="small">
+					<SendIcon />
+				</IconButton>
+			</Stack>
+
+			<Stack direction="row">
+				<ButtonGroup variant="outlined" orientation="vertical" size={"small"}>
+					<Button
+						onClick={() => {
+							alert("Left Clicked");
+						}}
+					>
+						Left
+					</Button>
+					<Button>Center</Button>
+					<Button>Right</Button>
+				</ButtonGroup>
+			</Stack>
 		</Stack>
 	);
 };
