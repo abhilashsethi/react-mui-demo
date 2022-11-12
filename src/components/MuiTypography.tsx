30d7ab4c679f9e55c884c211a82e96326cad8ec8
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2022-11-12 06:53:07 +0530

    typography

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,32 @@
+import { Typography } from "@mui/material";
+
+const MuiTypography = () => {
+	return (
+		<div>
+			<Typography variant="h1">h1 heading</Typography>
+			<Typography variant="h2">h2 heading</Typography>
+			<Typography variant="h3">h3 heading</Typography>
+			<Typography variant="h4">h4 heading</Typography>
+			<Typography variant="h5">h5 heading</Typography>
+			<Typography variant="h6">h6 heading</Typography>
+
+			<Typography variant="subtitle1">subtitle1</Typography>
+			<Typography variant="subtitle2">subtitle2</Typography>
+
+			<Typography variant="body1">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis
+				omnis quisquam laudantium dignissimos non accusantium, blanditiis error
+				deleniti aliquam ipsam doloremque tempora cum ullam itaque rem
+				repellendus fugiat minus repudiandae.
+			</Typography>
+			<Typography variant="body2">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis
+				omnis quisquam laudantium dignissimos non accusantium, blanditiis error
+				deleniti aliquam ipsam doloremque tempora cum ullam itaque rem
+				repellendus fugiat minus repudiandae.
+			</Typography>
+		</div>
+	);
+};
+
+export default MuiTypography;
